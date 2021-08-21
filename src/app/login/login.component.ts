b5a5d8ddb8714291983f205cac77ed1da9274993
Sandip Ghadge <ghadagesandip@gmail.com>
2021-08-21 22:28:44 +0530

    created template login form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,39 @@
-import { Component } from "@angular/core";
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { NgForm } from "@angular/forms";
+
+export interface ILogin {
+    username:string;
+    password:string;
+  } 
 
 @Component({
     selector: 'app-login',
     templateUrl: './login.component.html',
     styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent  implements OnInit {
 
+    @ViewChild('loginForm') loginForm: NgForm | undefined;
+    
+    isLoading:boolean = false;
+    loginModel:ILogin;
+
+    submitted = false;
+   
+    constructor(){
+       this.loginModel = {
+           username: '',
+           password: ''
+       }
+    }
+
+    ngOnInit() {
+     
+    }
+
+
+    onSubmit(formData: any){
+        console.log('this.userModel ', formData.value )
+        this.isLoading = true;
+    }
 }
--- src/app/primeng-design/primeng-design.module.ts
+++ src/app/primeng-design/primeng-design.module.ts
@@ -6,13 +6,16 @@ import { ButtonModule } from "primeng/button";
 import { CheckboxModule } from "primeng/checkbox";
 import { RadioButtonModule } from "primeng/radiobutton";
 import { FormsModule } from "@angular/forms";
+import { CardModule } from 'primeng/card';
 
 const classes = [
+  
+  FormsModule,
+  CardModule,
   ButtonModule,
   InputTextModule,
   CheckboxModule,
   RadioButtonModule,
-  FormsModule,
 ]
 @NgModule({
   declarations: [],
