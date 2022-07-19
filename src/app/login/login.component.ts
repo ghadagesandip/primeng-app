5b9d30822053659a6a72ccd6bbae4f82e415d6e6
Sandip Ghadge <ghadagesandip@gmail.com>
2022-07-19 22:34:23 +0530

    removed unwanted code

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -9,8 +9,6 @@ import {
 } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { select, Store } from '@ngrx/store';
-import moment, { now } from 'moment';
-import momentTz from 'moment-timezone';
 
 import {
   Login_Error,
@@ -67,38 +65,7 @@ export class LoginComponent implements OnInit, OnChanges, DoCheck {
     console.log('changes', changes);
   }
 
-  ngOnInit() {
-    console.log('ngOnInit called');
-    const zone = 'America/New_York';
-    const format = 'MM/DD/YYYY H:m A';
-
-    const mDate = momentTz.tz(new Date(), zone);
-    const mstr = mDate.format(format);
-    console.log('m date string', mstr);
-    const dateObj = new Date(mstr);
-    console.log('date obj', dateObj);
-
-    // authgear
-    //   .configure({
-    //     // custom domain endpoint or default endpoint
-    //     // default domain should be something like: https://<yourapp>.authgearapps.com
-    //     endpoint: 'https://sandiptest.authgearapps.com/',
-    //     // Client ID of your application
-    //     clientID: '8ab2a64f3e2e5e19',
-    //     // sessionType can be "refresh_token" or "cookie", default "refresh_token"
-    //     sessionType: 'refresh_token',
-    //   })
-    //   .then(
-    //     (res) => {
-    //       console.log('succ', res);
-    //       // configure successfully.
-    //     },
-    //     (err) => {
-    //       console.log('failed', err);
-    //       // failed to configure.
-    //     }
-    //   );
-  }
+  ngOnInit() {}
 
   ngDoCheck() {
     console.log('ngDoCheck');
@@ -107,27 +74,6 @@ export class LoginComponent implements OnInit, OnChanges, DoCheck {
   onSubmit() {
     this.submitted = true;
     if (this.loginForm && this.loginForm.valid) {
-      // authgear
-      //   .startAuthorization({
-      //     // configure redirectURI which users will be redirected to
-      //     // after they have authenticated with Authgear
-      //     // you can use any path in your website
-      //     // make sure it is in the "Redirect URIs" list of the Application
-      //     redirectURI: 'http://localhost:4200/dashboard',
-      //     prompt: 'login',
-      //   })
-      //   .then(
-      //     (res) => {
-      //       console.log('succ 1', res);
-      //       // started authorization, user should be redirected to Authgear
-      //     },
-      //     (err) => {
-      //       console.log('err 1', err);
-
-      //       // failed to start authorization
-      //     }
-      //   );
-
       this.store.dispatch(
         Login_Request({
           email: this.loginModel.email,
